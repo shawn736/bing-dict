9fda1103589fbd0944ba8f3abfe81d52082dee53
Shawn <xwh736@163.com>
2019-10-19 06:47:05 +0800

    只输出释义，去掉发音

--- dic.sh
+++ dic.sh
@@ -6,14 +6,15 @@ curl -SsL "http://cn.bing.com/dict/search?q=$word" |\
 	sed -E 's/<meta name="description" content="必应词典为您提供.+的释义，(.+)" ?\/>/\1/' |\
 	sed -E 's/(.*)(，)(.*)/\1 \3/' | awk -v WORD=$word '{
 		c=0;
-		print WORD;
 		for(i=1;i<=NF;i++) {
 			if(match($i, "^[a-z]+\\.|网络释义：$") != 0) {
-				if(c==0) printf "\n";
-				printf "\n";
+				if(c!=0) printf "\n";
 				c++;
 			}
-			printf "%s ",$i;
+      # 只输出释义，去掉发音
+      if(match($i, "美\\[") == 0 || match($i, "英\\[") == 0) {
+        printf "%s ",$i;
+      }
 		}
 		printf "\n";
 	}'
